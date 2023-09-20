71e17063ebe3a50a5997c44f1665d90ca977ef5f
Iamneo <techadmin@iamneo.ai>
2023-09-20 16:24:16 +0000

    Backup Commit

--- dotnetapp/Models/SlotBookingException.cs
+++ dotnetapp/Models/SlotBookingException.cs
@@ -3,5 +3,8 @@ namespace dotnetapp.Exceptions
     public class SlotBookingException    
     {
         public string SlotBookingExceptionMessage { get; set; }
+
+        public static readonly string SLOT_IS_FULL = "Slot is full.";
+        public static readonly string YOU_HAVE_ALREADY_BOOKED_THIS_SLOT = "You have already booked this slot."; 
     }
 }
