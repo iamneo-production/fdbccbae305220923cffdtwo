6b08ef3a360f7322a8d7e081d31b7bc9e327bd8f
Iamneo <techadmin@iamneo.ai>
2023-09-20 15:50:04 +0000

    Backup Commit

--- dotnetapp/Models/Slot.cs
+++ dotnetapp/Models/Slot.cs
@@ -2,11 +2,9 @@ namespace dotnetapp.Models
 {
     public class Slot
     {
-
-    public int SlotID { get; set; }
-    public string Time { get; set; }    
-    public double Duration { get; set; }    
-    public int Capacity { get; set; }
-
+        public int SlotID { get; set; }
+        public string Time { get; set; }    
+        public double Duration { get; set; }    
+        public int Capacity { get; set; }
     }
 }
