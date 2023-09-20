e002243f5350e2b074c20ed13bcf0d9c2fa402bd
Iamneo <techadmin@iamneo.ai>
2023-09-20 15:33:41 +0000

    Backup Commit

--- dotnetapp/Models/Slot.cs
+++ dotnetapp/Models/Slot.cs
@@ -1,5 +1,12 @@
-public class Slot
-public int SlotID { get; set; }
-public string Time { get; set; }    
-public double Duration { get; set; }    
-public int Capacity { get; set; }
+namespace dotnetapp.Models
+{
+    public class Slot
+    {
+
+    public int SlotID { get; set; }
+    public string Time { get; set; }    
+    public double Duration { get; set; }    
+    public int Capacity { get; set; }
+
+    }
+}
---
+++ dotnetapp/Models/SlotBookingException.cs
@@ -0,0 +1,7 @@
+namespace dotnetapp.Models
+{
+    public class Booking    
+    {
+        public string SlotBookingExceptionMessage { get; set; }
+    }
+}
