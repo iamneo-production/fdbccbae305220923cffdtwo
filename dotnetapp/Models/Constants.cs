1499b3446f64bbf76063bfaa7c94cfbc75f0d881
Iamneo <techadmin@iamneo.ai>
2023-09-21 09:52:42 +0000

    Backup Commit

---
+++ dotnetapp/Models/Constants.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetapp.Models
+{
+    public static class Constants
+    {
+        public static readonly string SLOT_IS_FULL = "Slot is full.";
+        public static readonly string YOU_HAVE_ALREADY_BOOKED_THIS_SLOT = "You have already booked this slot.";
+    }
+}
