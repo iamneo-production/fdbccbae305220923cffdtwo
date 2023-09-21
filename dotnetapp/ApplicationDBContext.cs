259acbcfcfeb488999e7bde5e2c636fca833d150
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:04:25 +0000

    Backup Commit

--- dotnetapp/ApplicationDBContext.cs
+++ dotnetapp/ApplicationDBContext.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; 
 
 namespace dotnetapp
 {
-    public class ApplicationDBContext
+    public class ApplicationDBContext //: DbContext  
     {
-        
+        // public ApplicationDBContext(DbContextOptions options) : base(options)  
+        // {  
+        // }  
+  
+        // DbSet<Booking> Bookings { get; set; }  
+        // DbSet<Slot> Slots { get; set; }    
     }
 }
